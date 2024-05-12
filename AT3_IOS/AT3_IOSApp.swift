dbe0a4841f964ed6b6a19b7f993d3dd8c9ed8835
CursedTeaa <163519582+CursedTeaa@users.noreply.github.com>
2024-05-12 22:52:06 +1000

    Merge branch 'main' of https://github.com/Dan-Gabb-UTS/AT3_IOS

--- AT3_IOS/AT3_IOSApp.swift
+++ AT3_IOS/AT3_IOSApp.swift
@@ -11,14 +11,15 @@ import GooglePlaces
 @main
 struct AT3_IOSApp: App {
     let favouritesModel = FavouritesModel()
+    
     // Paste the API key that I sent in the group chat here or the app wont function at all
     init() {
-        GMSPlacesClient.provideAPIKey("")
+        GMSPlacesClient.provideAPIKey("AIzaSyCa4_UJOJY6QS-B23VmF96Fzi4nj7SjCFI")
     }
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(image: Image("Icon"))
                 .environmentObject(favouritesModel)
         }
     }
--- AT3_IOS/ContentView.swift
+++ AT3_IOS/ContentView.swift
@@ -9,46 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var favouritesModel: FavouritesModel
+    var image: Image
+
     var body: some View {
         NavigationView {
             VStack {
-                Text("App Name Goes Here")
-                    .font(.largeTitle)
-                    .padding()
-
+                HStack {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                       // .padding(leading(50))
+                    
+                    
+                    
+                    Text("Main Menu")
+                        .bold()
+                        .font(.title)
+                        .padding(EdgeInsets())
+                }
+                .background(Color.green)
 
                 Spacer()
                 
                 // This links to the search functionality
                 NavigationLink(destination: TextSearchView()) {
-                    Text("Search for nearby food")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(8)
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.white)
+                        Text("Search food near me")
+                            .foregroundColor(.white)
+
+                    }
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(8)
+
                 }
                 .padding()
 
                 
                 // This links to the favourites list
                 NavigationLink(destination: FavouritesView()) {
-                    Text("Favorite Restaurants")
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.green)
-                        .cornerRadius(8)
+                    HStack{
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.white)
+                        Text("Favorite Restaurants")
+                            .foregroundColor(.white)
+
+                    }
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(8)
+
                 }
 
                 .padding()
 
                 Spacer()
             }
-            .navigationTitle("Main Menu")
                 Spacer()
           }
         }
     }
 
 #Preview {
-    ContentView()
+    ContentView(image: Image("Icon"))
 }
